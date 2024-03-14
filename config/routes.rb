59259824caeeb4c0a778ff1b9e5f594eab6edc05
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-03-14 12:37:47 +0000

    3/14 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'homes/top' , to: 'home#top'
+  constraints(subdomain: /(bookers-level1)/) do
+    get '/' , to:'home#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
